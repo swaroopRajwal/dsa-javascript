2543f1ac047d932c8acaa26fe5f229a3f169ad79
swaroop18122002 <swaroop.rajwal18@gmail.com>
2022-03-23 15:15:31 +0530

    🎉 initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import BST from "./lib/binary-tree";
+
+const myBST = new BST();
+
+myBST.insert(10);
+myBST.insert(12);
+myBST.insert(57);
+myBST.insert(57);
+myBST.insert(8);
+
+console.log(myBST);
